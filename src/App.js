248f897c21930d090894879029ab30da0be30aad
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-01-20 14:35:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FilterPractice from './components/FilterPractice'
+
+const App = () => {
+  return (
+    <>
+      <FilterPractice />
+    </>
+  )
+}
+
+export default App
