6fb0712e855c21d82ea72ccce537344bf91637ce
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-01-23 09:22:46 +0530

    change spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,14 +16,8 @@ module.exports = {
           "2xl": "1328px"
         },
       },
-
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"]
-      },
-      fontSize: {
-        custom2xl: "28px",
-        "custom-3xl": "32px",
-        "custom-sm": "12px"
       },
       lineHeight: {
        " custom-2xl":"121%"
